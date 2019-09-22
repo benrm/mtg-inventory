b5871f033ced74c5ef70d06aaf5438b9bfe9a364
Ben Magee <benrm@umich.edu>
2019-09-22 17:15:27 -0400

    Add tables for tracking transfers, use these to track cards

--- inventory.sql
+++ inventory.sql
@@ -13,9 +13,7 @@ CREATE TABLE IF NOT EXISTS singles (
 	expansion VARCHAR(256),
 	language VARCHAR(256),
 	owner VARCHAR(256) NOT NULL,
-	holder VARCHAR(256) NOT NULL,
-	FOREIGN KEY (owner) REFERENCES users(username),
-	FOREIGN KEY (holder) REFERENCES users(username)
+	FOREIGN KEY (owner) REFERENCES users(username)
 );
 
 CREATE TABLE IF NOT EXISTS requests (
@@ -31,4 +29,22 @@ CREATE TABLE IF NOT EXISTS requested_cards (
 	quantity INT NOT NULL,
 	UNIQUE(request_id, oracle_id),
 	FOREIGN KEY (request_id) REFERENCES requests(id)
+);
+
+CREATE TABLE IF NOT EXISTS transfers (
+	id INT NOT NULL PRIMARY KEY,
+	from_user VARCHAR(256) NOT NULL,
+	to_user VARCHAR(256) NOT NULL,
+	created DATETIME NOT NULL,
+	executed DATETIME,
+	request_id INT,
+	FOREIGN KEY (from_user) REFERENCES users(username),
+	FOREIGN KEY (to_user) REFERENCES users(username)
+);
+
+CREATE TABLE IF NOT EXISTS transferred_singles (
+	transfer_id INT NOT NULL,
+	single_id INT NOT NULL,
+	FOREIGN KEY (transfer_id) REFERENCES transfers(id),
+	FOREIGN KEY (single_id) REFERENCES singles(id)
 );
