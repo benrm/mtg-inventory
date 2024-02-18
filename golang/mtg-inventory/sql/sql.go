e1d048d55cbf5a28783b5036b97c00d209aa16bf
Benjamin Magee <benrm@umich.edu>
2024-02-18 14:07:29 -0500

    Move SQL into subdirectory, add check for environment variable to delete all rows for testing

--- golang/mtg-inventory/sql/sql.go
+++ golang/mtg-inventory/sql/sql.go
@@ -1,4 +1,4 @@
-package inventory
+package sql
 
 import (
 	"context"
--- golang/mtg-inventory/sql/sql_test.go
+++ golang/mtg-inventory/sql/sql_test.go
@@ -1,15 +1,26 @@
-package inventory
+package sql
 
 import (
 	"context"
 	"database/sql"
 	"os"
+	"strconv"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func TestSQL(t *testing.T) {
+	var err error
+	var deleteAll bool
+	deleteAllStr := os.Getenv("TEST_DELETE_ALL_ROWS")
+	if deleteAllStr != "" {
+		deleteAll, err = strconv.ParseBool(deleteAllStr)
+		if err != nil {
+			t.Fatalf("Failed to parse TEST_DELETE_ALL_ROWS: %s", err.Error())
+		}
+	}
+
 	dsn := os.Getenv("TEST_MYSQL_DSN")
 	if dsn == "" {
 		t.Skip("TEST_MYSQL_DSN is not set, skipping SQL tests")
@@ -17,6 +28,33 @@ func TestSQL(t *testing.T) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		t.Fatalf("Failed to open db connection: %s", err.Error())
+	}
+
+	if deleteAll {
+		_, err = db.Exec("DELETE FROM transferred_cards")
+		if err != nil {
+			t.Fatalf("Failed to delete from transferred_cards")
+		}
+		_, err = db.Exec("DELETE FROM transfers")
+		if err != nil {
+			t.Fatalf("Failed to delete from transfers")
+		}
+		_, err = db.Exec("DELETE FROM requested_cards")
+		if err != nil {
+			t.Fatalf("Failed to delete from requested_cards")
+		}
+		_, err = db.Exec("DELETE FROM requests")
+		if err != nil {
+			t.Fatalf("Failed to delete from requests")
+		}
+		_, err = db.Exec("DELETE FROM cards")
+		if err != nil {
+			t.Fatalf("Failed to delete from cards")
+		}
+		_, err = db.Exec("DELETE FROM users")
+		if err != nil {
+			t.Fatalf("Failed to delete from users")
+		}
 	}
 
 	var user *User
